940a46fe00336f45a9c8f7b0da2e9bddd03e7996
KarunDatadien <karun.datadien@wur.nl>
2025-01-31 13:46:19 +0100

    [ISSUE #1] rename physics functions

--- physics.jl
+++ physics.jl
@@ -1,7 +1,7 @@
 using .PhysicalConstants
 
 # Vapor Pressure Deficit Calculation
-function calculate_vpd_gpu(tair::CuArray{Float32}, vp::CuArray{Float32})
+function calculate_vpd(tair::CuArray{Float32}, vp::CuArray{Float32})
     svp = svp_a .* exp.((svp_b .* tair) ./ (svp_c .+ tair))
     svp = ifelse.(tair .< 0, svp .* (1.0 .+ 0.00972 .* tair .+ 0.000042 .* tair.^2), svp)
     vpd = svp .- vp
@@ -9,16 +9,16 @@ function calculate_vpd_gpu(tair::CuArray{Float32}, vp::CuArray{Float32})
 end
 
 # Saturation Vapor Pressure Slope
-function calculate_svp_slope_gpu(temp::CuArray{Float32})
-    return (svp_b * svp_c) ./ ((svp_c .+ temp).^2) .* (svp_a .* exp.((svp_b .* temp) ./ (svp_c .+ temp)))
+function calculate_svp_slope(temp::CuArray{Float32})
+    return (svp_b .* svp_c) ./ ((svp_c .+ temp).^2) .* (svp_a .* exp.((svp_b .* temp) ./ (svp_c .+ temp)))
 end
 
 # Scale Height Calculation
-function calculate_scale_height_gpu(tair::CuArray{Float32}, elevation::CuArray{Float32})
-    return r_air / g .* ((tair .+ t_freeze) .+ 0.5 .* elevation .* lapse_rate)
+function calculate_scale_height(tair_gpu, elev)
+    return r_air ./ g .* ((tair_gpu .+ t_freeze) .+ 0.5 .* elev .* lapse_rate)
 end
 
 # Latent Heat of Vaporization
-function calculate_latent_heat_gpu(temp::CuArray{Float32})
+function calculate_latent_heat(temp::CuArray{Float32})
     return lat_vap .- 2361.0 .* temp
 end
