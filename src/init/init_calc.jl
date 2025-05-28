80a3c7fd70622cc366cc64d6ec01e4cef4a33a62
Karun <karun.datadien@wur.nl>
2025-05-28 14:43:43 +0200

    [ISSUE #9] add correct residual moisture calculation

--- src/init/init_calc.jl
+++ src/init/init_calc.jl
@@ -1,4 +1,4 @@
-function calculate_soil_properties(bulk_dens_gpu, soil_dens_gpu, depth_gpu, Wcr_gpu, Wfc_gpu, Wpwp_gpu)
+function calculate_soil_properties(bulk_dens_gpu, soil_dens_gpu, depth_gpu, Wcr_gpu, Wfc_gpu, Wpwp_gpu, residmoist_gpu)
 
     # === Calculate Bulk Density and Porosity ===
     bulk_dens_min = (bulk_dens_gpu .- organic_frac * bulk_dens_org) ./ (1 .- organic_frac)
@@ -15,6 +15,7 @@ function calculate_soil_properties(bulk_dens_gpu, soil_dens_gpu, depth_gpu, Wcr_
     soil_moisture_critical = Wcr_gpu .* soil_moisture_max
     field_capacity = Wfc_gpu .* soil_moisture_max
     wilting_point = Wpwp_gpu .* soil_moisture_max
+    residual_moisture = residmoist_gpu .* depth_gpu .* 1000
 
-    return bulk_dens_min, soil_dens_min, porosity, soil_moisture_max, soil_moisture_critical, field_capacity, wilting_point
+    return bulk_dens_min, soil_dens_min, porosity, soil_moisture_max, soil_moisture_critical, field_capacity, wilting_point, residual_moisture
 end
