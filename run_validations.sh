033acc3785b3c78f9ab8fc90d996754888c22927
Karun <karun.datadien@wur.nl>
2026-06-15 12:56:32 +0200

    [ISSUE #26] add netcdf output cmndline argument to CI runner

--- run_validations.sh
+++ run_validations.sh
@@ -1,6 +1,6 @@
-julia --project=. run.jl configs/mekong_config.toml 1979 1980
-#julia --project=. run.jl configs/indus_config.toml
+julia --project=. run.jl configs/mekong_config.toml 1979 1980 --nc
+#julia --project=. run.jl configs/indus_config.toml --nc
 
 cd ./validations
-#python plot_dashboards_mekong_indus.py
-python plot_dashboards_mekong.py
+#python3 plot_dashboards_mekong_indus.py
+python3 plot_dashboards_mekong.py
