a465b28c8fb041cbeb145fa35a3169a11437802a
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2026-07-27 11:03:02 +0200

    Use improved python script in validation runs

--- run_validations.sh
+++ run_validations.sh
@@ -1,6 +1,4 @@
 julia --project=. run.jl configs/mekong_config.toml 1979 1980 --nc
 #julia --project=. run.jl configs/indus_config.toml --nc
 
-cd ./validations
-#python3 plot_dashboards_mekong_indus.py
-python3 plot_dashboards_mekong.py
+python3 validations/plot_dashboards.py mekong
