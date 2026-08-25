08456d6c0e0ab8700cc5a42c0ca78690d0189c1d
Karun <karun.datadien@wur.nl>
2026-08-25 16:38:42 +0200

    CI output in nc format

--- run_validations.sh
+++ run_validations.sh
@@ -1,6 +1,6 @@
 set -e
 
-julia --project=. -e 'using mGV; mGV.run()' configs/mekong_config.toml --nc
-#julia --project=. -e 'using mGV; mGV.run()' configs/indus_config.toml --nc
+julia --project=. -e 'using mGV; mGV.run()' -- configs/mekong_config.toml
+#julia --project=. -e 'using mGV; mGV.run()' -- configs/indus_config.toml --format=$FORMAT
 
 python3 validations/plot_dashboard.py mekong
