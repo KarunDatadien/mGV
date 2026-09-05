7e3fa4392e3f51f563119644cae6d2c8ec4f62db
KarunDatadien <karun.datadien@wur.nl>
2026-09-05 19:28:35 +0200

    Bugfix for monthly parameter loading

--- run_validations.sh
+++ run_validations.sh
@@ -1,6 +1,6 @@
 set -e
 
 julia --project=. -e 'using mGV; mGV.run()' -- configs/mekong_config.toml
-julia --project=. -e 'using mGV; mGV.run()' -- configs/indus_config.toml 
+julia --project=. -e 'using mGV; mGV.run()' -- configs/indus_config.toml
 
 python3 validations/plot_dashboard.py mekong
